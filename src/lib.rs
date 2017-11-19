78c0621731c54a4fcd3aa8e84fe542db6c0c986a
Taro Yamashita <taro.ymst@gmail.com>
2017-11-20 08:12:15 +0900

    Write documents.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! # dogrun
+//!
+//! The dog eats everthing.
 use std::collections::BTreeMap;
 use std::fmt;
 use std::fs;
@@ -12,7 +15,9 @@ pub type DogResult<T> = Result<T, DogError>;
 
 #[derive(Debug)]
 pub enum DogError {
+    /// No specified targets
     EmptyFood,
+    /// Files of targets are not writable
     Uneatable(BadFood),
 }
 
@@ -34,9 +39,13 @@ impl fmt::Display for DogError {
 
 #[derive(Debug)]
 pub enum TastingError {
+    /// Something is wrong
     Confused,
+    /// Permission denied
     NotAllowed,
+    /// File does not exist
     NotExist,
+    /// Not a regular-file
     NotFood,
 }
 
@@ -53,10 +62,21 @@ impl fmt::Display for TastingError {
 
 #[derive(Debug)]
 pub struct Args {
+    /// Arguments of specified
     args: Vec<String>,
 }
 
 impl Args {
+    /// Create an `Args`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use dogrun::Args;
+    ///
+    /// let args = Args::new(vec!["a".into(), "b".into()]);
+    /// assert_eq!(args.args().len(), 1);
+    /// ```
     pub fn new(args: Vec<String>) -> Self {
         let collect = args.iter()
             .skip(1)
@@ -66,6 +86,16 @@ impl Args {
         Args { args: collect }
     }
 
+    /// Get targets.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use dogrun::Args;
+    ///
+    /// let args = Args::new(vec!["a".into(), "b".into()]);
+    /// assert_eq!(args.args()[0], "b");
+    /// ```
     pub fn args(&self) -> Vec<String> {
         self.args.clone()
     }
@@ -73,10 +103,22 @@ impl Args {
 
 #[derive(Debug)]
 pub struct Dog {
+    /// Targets of files
     foods: Vec<String>,
 }
 
 impl Dog {
+    /// Create a `Dog`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use dogrun::{Args, Dog};
+    ///
+    /// let args = Args::new(vec!["a".into(), "b".into()]);
+    /// let dog = Dog::foods(args);
+    /// assert!(dog.is_ok());
+    /// ```
     pub fn foods(args: Args) -> DogResult<Dog> {
         let foods = args.args();
         if foods.is_empty() {
@@ -86,6 +128,15 @@ impl Dog {
         }
     }
 
+    /// Execute a command.
+    ///
+    /// `Dog` returns an error when foods could not eat.
+    ///
+    /// - Does not exist
+    /// - Permission denied
+    /// - Directory
+    /// - Read-only permission
+    /// - Not a regular-file
     pub fn run(&self) -> DogResult<()> {
         let tasting = self.wait();
         match tasting.len() {
